1bc0c2f4ec95f8e486ce41de36cf649e2cc89f02
implodingduck <zsek320@gmail.com>
2021-07-21 00:36:44 -0500

    ading cosmos

--- main.tf
+++ main.tf
@@ -36,3 +36,42 @@ resource "random_string" "unique" {
   special = false
   upper   = false
 }
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "${local.func_name}-dba"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  enable_free_tier    = true
+  consistency_policy {
+    consistency_level       = "Session"
+  }
+
+  geo_location {
+    location          = "West US"
+    failover_priority = 1
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "db" {
+  name                = "${local.func_name}-db"
+  resource_group_name = azurerm_cosmosdb_account.db.resource_group_name
+  account_name        = azurerm_cosmosdb_account.db.name
+  throughput          = 400
+}
+
+
+resource "azurerm_cosmosdb_sql_container" "db" {
+  name                  = "${local.func_name}-dbcontainer"
+  resource_group_name   = azurerm_cosmosdb_account.db.resource_group_name
+  account_name          = azurerm_cosmosdb_account.db.name
+  database_name         = azurerm_cosmosdb_sql_database.db.name
+  partition_key_path    = "/id"
+  partition_key_version = 1
+  throughput            = 400
+}
